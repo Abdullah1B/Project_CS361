d534f46c9c9874fa0a88ec8833738f46c1d0aad1
Abdullah1B <a.j.baskran@gmail.com>
2021-11-28 20:45:43 +0300

    added heuristic function

--- Node.py
+++ Node.py
@@ -1,14 +1,21 @@
 class Node(object):
     def __init__(self,Stick_a, Stick_b, Stick_c, parent, goal_node= False):
-        self.Stick = ([e for e in Stick_a],[e for e in Stick_b],[e for e in Stick_c])
+        self.Sticks = ([e for e in Stick_a],
+                       [e for e in Stick_b],
+                       [e for e in Stick_c])
         self.parent = parent
         self.goal_node = goal_node
         self.Fvalue = None
-   
-    def f(self):# f(n) = g(n) + h(n)
-        self.Fvalue = self.backward_cost_g() + self.forward_cost_h()
+    
+    def f(self,cost,goal_node,target):# f(n) = g(n) + h(n)
+        self.Fvalue = self.backward_cost_g(cost) + self.forward_cost_heuristic(goal_node,target)
         return self.Fvalue
-    def backward_cost_g(self): # g(n)
-        pass
-    def forward_cost_h(self):# h(n) 
-        pass
+
+    def backward_cost_g(self,cost): # g(n)
+        return cost + 1
+        
+    def forward_cost_heuristic(self,goal_node,target):# h(n) heuristic is the number of misplaced of disks in the target stick.
+        if target == 2:
+            return len(goal_node.Sticks[1]) - len(self.Sticks[1]) # targt stick in the middle
+        elif target == 3: 
+            return len(goal_node.Sticks[2] - len(self.Sticks[2])) # target stick the last one 
