f56106464d3235a1112feef69736708bde7373e1
Alawashez2 <94991478+Alawashez2@users.noreply.github.com>
2021-12-07 01:01:40 +0300

    Update Node.py

--- Node.py
+++ Node.py
@@ -16,6 +16,8 @@ class Node(object):
         
     def forward_cost_heuristic(self,goal_node,target):# h(n) heuristic is the number of misplaced of disks in the target stick.
         if target == 2:
+            #return (len(goal_node.Towers[:]) + 2) % 2
             return len(goal_node.Towers[1]) - len(self.Towers[1]) # targt stick in the middle
-        elif target == 3: 
+        elif target == 3:
+            #return (len(goal_node.Towers[:]) + 3)%2
             return len(goal_node.Towers[2]) - len(self.Towers[2]) # target stick the last one 
